cf45f7d5104cd7b737a0224004a9fe334d5573fc
SamHeutmaker <samheutmaker@gmail.com>
2018-09-08 14:51:05 -0600

    add handlers for user and rooms

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
-const btoa = require('btoa');
-const request = require('superagent');
+const btoa = require("btoa");
+const request = require("superagent");
 
 class Client {
-  constructor(baseUrl = '') {
+  constructor(baseUrl = "") {
     this.baseUrl = baseUrl;
     this.token = null;
   }
@@ -12,12 +12,12 @@ class Client {
   }
   _defaultHeaders() {
     let headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json"
     };
     if (this.token) {
-      headers.token = this.token
-    };
+      headers.token = this.token;
+    }
     return headers;
   }
   _setToken(token) {
@@ -27,7 +27,7 @@ class Client {
     throw error;
   }
   _handleHTTPError(error) {
-    console.error(error)
+    console.log(error);
     let msg;
     try {
       msg = JSON.parse(error.response.text);
@@ -37,7 +37,7 @@ class Client {
     throw msg;
   }
   // Requests
-  get(slug = '', headers = {}) {
+  get(slug = "", headers = {}) {
     return request
       .get(this._getUrl(slug))
       .set({
@@ -46,7 +46,7 @@ class Client {
       })
       .catch(this._handleHTTPError);
   }
-  post(slug = '', data = {}, headers = {}) {
+  post(slug = "", data = {}, headers = {}) {
     return request
       .post(this._getUrl(slug))
       .set({
@@ -56,7 +56,7 @@ class Client {
       .send(data)
       .catch(this._handleHTTPError);
   }
-  put(slug = '', data = {}, headers = {}) {
+  put(slug = "", data = {}, headers = {}) {
     return request
       .put(this._getUrl(slug))
       .set({
@@ -66,7 +66,7 @@ class Client {
       .send(data)
       .catch(this._handleHTTPError);
   }
-  delete(slug = '', data = {}, headers = {}) {
+  delete(slug = "", data = {}, headers = {}) {
     return request
       .delete(this._getUrl(slug))
       .set({
@@ -77,21 +77,11 @@ class Client {
       .catch(this._handleHTTPError);
   }
   // Authentication
-  login(email, password) {
-    let authorization = 'Basic ' + btoa(`${email}:${password}`);
-    return this
-      .get('/auth/login', {
-        authorization
-      })
-      .then(res => {
-        this._setToken(res.body.token);
-        return res.body;
-      })
-      .catch(this._handleError);
-  }
-  register(registerData) {
-    return this
-      .post('/user/register', registerData)
+  login(username, phoneNumber) {
+    return this.post("/user/login", {
+      username,
+      phoneNumber
+    })
       .then(res => {
         this._setToken(res.body.token);
         return res;
@@ -101,43 +91,53 @@ class Client {
       })
       .catch(this._handleError);
   }
-  // Addresses
-  createAddress(address) {
-    return this
-      .post('/address/new', address)
+  register(userName, phoneNumber) {
+    return this.post("/user/register", {
+      userName,
+      phoneNumber
+    })
+      .then(res => {
+        // this._setToken(res.body.token);
+        return res;
+      })
+      .then(res => {
+        return res.body;
+      })
+      .catch(this._handleError);
+  }
+  // Rooms
+  createRoom(title, owner) {
+    return this.post("/room/new", {
+      title,
+      owner
+    })
       .then(res => res.body)
       .catch(this._handleError);
   }
-  listAddresses() {
-    return this
-      .get('/address/list')
-      .then(res => res.body)
-      .catch(this._handleError);
-  }
-  updateAddress(address) {
-    return this
-      .put('/address/update', address)
-      .then(res => res.body)
-      .catch(this._handleError);
-  }
-  deleteAddress(id) {
-    return this
-      .delete('/address/delete', { id: id })
+  listRooms() {
+    return this.get("/room/list")
       .then(res => res.body)
       .catch(this._handleError);
   }
 }
 
-let client = new Client('http://localhost:8080');
-
+let client = new Client("http://localhost:8080");
 
 let user = {
-  username: 'sam',
-  phoneNumber: '4062104444'
+  _id: "5b933843bfde01490c9fbd88",
+  phoneNumberVerifiedAt: null,
+  username: "sam",
+  phoneNumber: "4062104444",
+  created: "2018-09-08T02:47:31.570Z"
 };
 
-client.register(user)
-.then(res => {
-  console.log(res);
-})
-.catch(err => console.error(err));
+
+
+  client.login(user.username, user.phoneNumber)
+  .then(res => client.createRoom("This is the first room", user._id))
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
